ceb522ca1b4c3419e22df373ffeae8d558079492
tonitakekawa <tonitakekawa@gmail.com>
2015-10-30 00:42:15 +0900

    複数ユーザーでのテスト

--- GameLogicTester/Program.cs
+++ GameLogicTester/Program.cs
@@ -18,15 +18,16 @@ namespace GameLogicTester
 
             var _urls = new[]
             {
-                head + "config?gameId=1?userId=1",
-                head + "init?gameId=1?userId=1",
-                head + "play?gameId=1?userId=1?betcount=1?rate=0.00",
-                head + "correct?gameId=1?userId=1?reelstopleft=0?reelstopright=0?reelstopcenter=0?oshijun=0",
+                head + "config?gameId={0}?userId={1}",
+                head + "init?gameId={0}?userId={1}",
+                head + "play?gameId={0}?userId={1}?betcount=1?rate=0.00",
+                head + "correct?gameId={0}?userId={1}?reelstopleft=0?reelstopright=0?reelstopcenter=0?oshijun=0",
             };
 
-            var urls = from url in _urls
-                       from count in Enumerable.Range(0, 100)
-                       select url;
+            var urls = from gameId in Enumerable.Range(2, 1)
+                       from userId in Enumerable.Range(1, 10)
+                       from url in _urls
+                       select String.Format(url, gameId, userId);
 
             var msg = String.Join(
                 Environment.NewLine,
@@ -34,17 +35,27 @@ namespace GameLogicTester
             );
 
             Console.WriteLine(msg);
+            Console.ReadLine();
         }
 
         string Url2Json(string url)
         {
-            var req = WebRequest.Create(url);
-            var res = req.GetResponse();
-            var st = res.GetResponseStream();
-            var sr = new StreamReader(st);
-            var json = sr.ReadToEnd();
-            sr.Close();
-            st.Close();
+            var json = null as string;
+
+            try
+            {
+                var req = WebRequest.Create(url);
+                var res = req.GetResponse();
+                var st = res.GetResponseStream();
+                var sr = new StreamReader(st);
+                json = sr.ReadToEnd();
+                sr.Close();
+                st.Close();
+            }
+            catch( Exception ex)
+            {
+                return ex.ToString();
+            }
 
             return json;
         }
